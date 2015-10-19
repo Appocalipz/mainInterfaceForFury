3566760ad78e73120e5b396532362ba0f86fe6c0
Melcom van Eeden <melcomvaneeden@Melcoms-MacBook-Pro.local>
2015-10-19 20:23:39 +0200

    fix

--- app/controllers/ComputerCraft.scala
+++ app/controllers/ComputerCraft.scala
@@ -4,7 +4,7 @@ import play.api.mvc._
 
 class ComputerCraft extends Controller {
   def get (id: String, index: String) = Action {
-    val lines = scala.io.Source.fromFile("assets/get/"+id+"/"+index+".lua").mkString("\n")
+    val lines = scala.io.Source.fromFile("app/assets/get/"+id+"/"+index+".lua").mkString("\n")
     Ok(views.html.index())
   }
 }
